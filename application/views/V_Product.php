90598f0a0f2085eac6bd93370608fb436681b2cb
Unknown <astefanus44@gmail.com>
2019-08-13 12:39:24 +0700

    [UPDATE] Product page

--- application/views/V_Product.php
+++ application/views/V_Product.php
@@ -1,97 +1,260 @@
-<div class="breadcrumb-area" data-black-overlay="7">
+
+        <!-- Breacrumb Area -->
+        <div class="breadcrumb-area" data-black-overlay="7">
             <div class="container">
                 <div class="row">
                     <div class="col-xl-8 offset-xl-2 col-lg-10 offset-lg-1">
                         <div class="cr-breadcrumb text-center">
-                            <h1>Our Products</h1>
+                            <h1>Product</h1>
+                            <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered
+                                alteration passages of Lorem Ipsum available, but the majority</p>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
+        <!--// Breacrumb Area -->
 
+        <!-- Page Content -->
+        <main class="page-content">
 
-<section id="blogs-area" class="blogs-area section-padding-lg bg-white">
+          
+
+            <!-- Services Area -->
+            <section class="services-area section-padding-lg bg-white">
                 <div class="container">
+
                     <div class="row">
                         <div class="col-xl-8 offset-xl-2 col-lg-10 offset-lg-1 col-12 offset-0">
                             <div class="section-title text-center">
-                                
-                                <h2>Product</h2>
+                                <!-- <h6>Believe In Work</h6> -->
+                                <h2>Tissue</h2>
                             </div>
                         </div>
                     </div>
-                    <div class="row justify-content-center">
 
-                        <!-- Single Blog -->
-                        <div class="col-lg-4 col-md-6">
-                            <article class="blog blog-style-2">
-                                <div class="blog-thumb">
-                                    <a href="blog-details.html">
-                                        <img src="images/blog/layout-1/blog-thumbnail-9.jpg" alt="blog thumb">
-                                    </a>
-                                </div>
-                                <div class="blog-content">
-                                    <div class="blog-meta">
-                                        <span>26 Nov 2017</span>
-                                        <span>By
-                                            <a href="blog-fullwidth.html">Momen</a>
-                                        </span>
-                                    </div>
-                                    <h5>
-                                        <a href="blog-details.html">A conversation about challenge</a>
-                                    </h5>
-                                </div>
-                            </article>
-                        </div>
-                        <!--// Single Blog -->
+                    <div class="row justify-content-center row justify-content-center service-slider-active cr-slider-dots-1">
 
-                        <!-- Single Blog -->
-                        <div class="col-lg-4 col-md-6">
-                            <article class="blog blog-style-2">
-                                <div class="blog-thumb">
-                                    <a href="blog-details.html">
-                                        <img src="images/blog/layout-1/blog-thumbnail-10.jpg" alt="blog thumb">
-                                    </a>
+                        <!-- Signle Service -->
+                        <div class="col-lg-4 col-md-6 col-12">
+                            <div class="service service-style-5">
+                                <div class="service-image">
+                                    <img src="<?php echo base_url();?>assets/images/ALL CK.jpg" alt="Service Thumb" style="height:300px">
                                 </div>
-                                <div class="blog-content">
-                                    <div class="blog-meta">
-                                        <span>21 Aug 2017</span>
-                                        <span>By
-                                            <a href="blog-fullwidth.html">Admin</a>
-                                        </span>
-                                    </div>
-                                    <h5>
-                                        <a href="blog-details.html">Some patience for the modern marketer</a>
-                                    </h5>
+                                <div class="service-content">
+                                    <h4>
+                                        <a href="service-details.html">Circle K</a>
+                                    </h4>
+                                    <!-- <p>There are many variations of passages Lorem Ipsum, but the majority variations of passages
+                                        of Lorem Ipsum
+                                    </p> -->
+                                    <!-- <a href="service-details.html" class="cr-readmore2">Read More</a> -->
                                 </div>
-                            </article>
+                            </div>
                         </div>
-                        <!--// Single Blog -->
+                        <!--// Signle Service -->
 
-                        <!-- Single Blog -->
-                        <div class="col-lg-4 col-md-6">
-                            <article class="blog blog-style-2">
-                                <div class="blog-thumb">
-                                    <a href="blog-details.html">
-                                        <img src="images/blog/layout-1/blog-thumbnail-5.jpg" alt="blog thumb">
-                                    </a>
+                        <!-- Signle Service -->
+                        <div class="col-lg-4 col-md-6 col-12">
+                            <div class="service service-style-5">
+                                <div class="service-image">
+                                    <img src="<?php echo base_url();?>assets/images/ALFA Tissue.jpg" alt="Service Thumb" style="height:300px">
                                 </div>
-                                <div class="blog-content">
-                                    <div class="blog-meta">
-                                        <span>13 Oct 2017</span>
-                                        <span>By
-                                            <a href="blog-fullwidth.html">Maksud</a>
-                                        </span>
-                                    </div>
-                                    <h5>
-                                        <a href="blog-details.html">We don’t meeting people by accident</a>
-                                    </h5>
+                                <div class="service-content">
+                                    <h4>
+                                        <a href="service-details.html">Alfamart</a>
+                                    </h4>
+                                   
                                 </div>
-                            </article>
+                            </div>
                         </div>
-                        <!--// Single Blog -->
+                        <!--// Signle Service -->
+
+                        <!-- Signle Service -->
+                        <div class="col-lg-4 col-md-6 col-12">
+                            <div class="service service-style-5">
+                                <div class="service-image">
+                                    <img src="<?php echo base_url();?>assets/images/Caris Disney.jpg" alt="Service Thumb" style="height:300px">
+                                </div>
+                                <div class="service-content">
+                                    <h4>
+                                        <a href="service-details.html">Caris Disney</a>
+                                    </h4>
+                                </div>
+                            </div>
+                        </div>
+                        <!--// Signle Service -->
+
+                        <!-- Signle Service -->
+                        <div class="col-lg-4 col-md-6 col-12">
+                            <div class="service service-style-5">
+                                <div class="service-image">
+                                    <img src="<?php echo base_url();?>assets/images/Foodhall.jpg" alt="Service Thumb" style="height:300px">
+                                </div>
+                                <div class="service-content">
+                                    <h4>
+                                        <a href="service-details.html">Foodhall</a>
+                                    </h4>
+                                   </div>
+                            </div>
+                        </div>
+                        <!--// Signle Service -->
+
+                        <!-- Signle Service -->
+                        <div class="col-lg-4 col-md-6 col-12">
+                            <div class="service service-style-5">
+                                <div class="service-image">
+                                    <img src="<?php echo base_url();?>assets/images/Federal 200's (2).jpg" alt="Service Thumb" style="height:300px">
+                                </div>
+                                <div class="service-content">
+                                    <h4>
+                                        <a href="service-details.html">Federal</a>
+                                    </h4>
+                                  
+                                </div>
+                            </div>
+                        </div>
+                        <!--// Signle Service -->
+
+                        <!-- Signle Service -->
+                        <div class="col-lg-4 col-md-6 col-12">
+                            <div class="service service-style-5">
+                                <div class="service-image">
+                                    <img src="<?php echo base_url();?>assets/images/IMG_3623.jpg" alt="Service Thumb" style="height:300px">
+                                </div>
+                                <div class="service-content">
+                                    <h4>
+                                        <a href="service-details.html">Festival</a>
+                                    </h4>
+                              
+                                </div>
+                            </div>
+                        </div>
+                        <!--// Signle Service -->
 
                     </div>
                 </div>
             </section>
+            <!--// Services Area -->
+
+  <!-- Services Area -->
+            <section class="services-area section-padding-lg bg-white">
+                <div class="container">
+
+                    <div class="row">
+                        <div class="col-xl-8 offset-xl-2 col-lg-10 offset-lg-1 col-12 offset-0">
+                            <div class="section-title text-center">
+                                <!-- <h6>Believe In Work</h6> -->
+                                <h2>Tissue Box</h2>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="row justify-content-center row justify-content-center service-slider-active cr-slider-dots-1">
+
+                        <!-- Signle Service -->
+                        <div class="col-lg-4 col-md-6 col-12">
+                            <div class="service service-style-5">
+                                <div class="service-image">
+                                    <img src="<?php echo base_url();?>assets/images/Box Blue Bird.jpg" alt="Service Thumb" style="height:300px">
+                                </div>
+                                <div class="service-content">
+                                    <h4>
+                                        <a href="service-details.html">Blue Bird</a>
+                                    </h4>
+                                    <!-- <p>There are many variations of passages Lorem Ipsum, but the majority variations of passages
+                                        of Lorem Ipsum
+                                    </p> -->
+                                    <!-- <a href="service-details.html" class="cr-readmore2">Read More</a> -->
+                                </div>
+                            </div>
+                        </div>
+                        <!--// Signle Service -->
+
+                        <!-- Signle Service -->
+                        <div class="col-lg-4 col-md-6 col-12">
+                            <div class="service service-style-5">
+                                <div class="service-image">
+                                    <img src="<?php echo base_url();?>assets/images/Box Natal.jpg" alt="Service Thumb" style="height:300px">
+                                </div>
+                                <div class="service-content">
+                                    <h4>
+                                        <a href="service-details.html">Chrismast Box</a>
+                                    </h4>
+                                   
+                                </div>
+                            </div>
+                        </div>
+                        <!--// Signle Service -->
+
+                        <!-- Signle Service -->
+                        <div class="col-lg-4 col-md-6 col-12">
+                            <div class="service service-style-5">
+                                <div class="service-image">
+                                    <img src="<?php echo base_url();?>assets/images/Box New Year.jpg" alt="Service Thumb" style="height:300px">
+                                </div>
+                                <div class="service-content">
+                                    <h4>
+                                        <a href="service-details.html">New Year Box</a>
+                                    </h4>
+                                </div>
+                            </div>
+                        </div>
+                        <!--// Signle Service -->
+
+                        <!-- Signle Service -->
+                        <div class="col-lg-4 col-md-6 col-12">
+                            <div class="service service-style-5">
+                                <div class="service-image">
+                                    <img src="<?php echo base_url();?>assets/images/Ketupat.jpg" alt="Service Thumb" style="height:300px">
+                                </div>
+                                <div class="service-content">
+                                    <h4>
+                                        <a href="service-details.html">Ketupat</a>
+                                    </h4>
+                                   </div>
+                            </div>
+                        </div>
+                        <!--// Signle Service -->
+
+                        <!-- Signle Service -->
+                        <div class="col-lg-4 col-md-6 col-12">
+                            <div class="service service-style-5">
+                                <div class="service-image">
+                                    <img src="<?php echo base_url();?>assets/images/Suzuki.jpg" alt="Service Thumb" style="height:300px">
+                                </div>
+                                <div class="service-content">
+                                    <h4>
+                                        <a href="service-details.html">Suzuki</a>
+                                    </h4>
+                                  
+                                </div>
+                            </div>
+                        </div>
+                        <!--// Signle Service -->
+
+                        <!-- Signle Service -->
+                        <div class="col-lg-4 col-md-6 col-12">
+                            <div class="service service-style-5">
+                                <div class="service-image">
+                                    <img src="<?php echo base_url();?>assets/images/IMG_3623.jpg" alt="Service Thumb" style="height:300px">
+                                </div>
+                                <div class="service-content">
+                                    <h4>
+                                        <a href="service-details.html">Festival</a>
+                                    </h4>
+                              
+                                </div>
+                            </div>
+                        </div>
+                        <!--// Signle Service -->
+
+                    </div>
+                </div>
+            </section>
+            <!--// Services Area -->
+         
+         
+
+        </main>
+        <!--// Page Content -->
