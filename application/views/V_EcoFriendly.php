9c6797f0d9da85b65048970620da25f51d5e5eb2
Unknown <astefanus44@gmail.com>
2019-08-22 01:42:44 +0700

    [ADD] Eco-friendly page

---
+++ application/views/V_EcoFriendly.php
@@ -0,0 +1,101 @@
+<div class="breadcrumb-area" data-black-overlay="7">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xl-8 offset-xl-2 col-lg-10 offset-lg-1">
+                        <div class="cr-breadcrumb text-center">
+                            <h1>Eco-friendly</h1>
+                           
+                           
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <main class="page-content">
+        <section class="section-title-showcase section-padding-lg bg-white">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-xl-8 offset-xl-2 col-lg-10 offset-lg-1 col-12 offset-0">
+                            <div class="section-title text-center">
+                              
+                                <h4 class="">Sesuai dengan nama perusahaan ini, kami percaya bahwa suatu industri dapat
+terus berkarya apabila selalu selaras dengan alam di mana kami berpijak.
+Tanggung jawab atas masa depan lingkungan ada di tangan kami, oleh sebab itu
+kami sedang mengembangkan teknologi untuk membuat produk yang ramah
+lingkungan. </h4>
+<br>
+<h3>Dalam waktu dekat akan memproduksi :</h3>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+                
+
+            <section class="services-area  bg-white">
+                <div class="container">
+
+                    <div class="row justify-content-center">
+
+                        <!-- Signle Service -->
+                        <div class="col-lg-6 col-md-6 col-12">
+                            <div class="service service-style-3">
+                                <div class="service-content">
+                                    <h4 class="text-center text-success">Green Party Ware</h4>
+                                    <p>Kebutuhan piring sekali pakai dalam suatu pesta masih
+dibutuhkan untuk meminimalisasi kesibukan tuan rumah.
+Kami sedang mencoba untuk memenuhi kebutuhan Anda
+seperti gelas, piring, sendok dan garpu sekali pakai tanpa
+plastik, tidak beracun.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <!--// Signle Service -->
+ <!-- Signle Service -->
+ <div class="col-lg-6 col-md-6 col-12">
+                            <div class="service service-style-3">
+                                <div class="service-content">
+                                    <h4 class="text-center text-success">Bamboo-based disposable plates</h4>
+                                    <p>Salah satu party ware yang sedang kami garap adalah piring dari bahan bambu, berasal dari alam sehingga lebih mudah terurai.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <!--// Signle Service -->
+
+                      <!-- Signle Service -->
+                        <div class="col-lg-6 col-md-6 col-12">
+                            <div class="service service-style-3">
+                                <div class="service-content">
+                                    <h4 class="text-center text-success">Unbleached tissue</h4>
+                                    <p>Tissue cokelat tanpa proses bleaching, lebih aman bagi tubuh karena tidak mengandung bahan kimia pemutih karsinogenik, sekaligus aman bagi lingkungan.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <!--// Signle Service -->
+ <!-- Signle Service -->
+ <div class="col-lg-6 col-md-6 col-12">
+                            <div class="service service-style-3">
+                                <div class="service-content">
+                                    <h4 class="text-center text-success">Corn-starch based tooth brush.</h4>
+                                    <p> Sikat gigi adalah produk kebersihan vital keluarga namun
+menjadi salah satu penyumbang plastik terbesar di dunia.
+Batang sikat gigi kami yang terbuat dari bonggol jagung
+menjadi solusi bagi Anda untuk menjaga keluarga higienis
+tanpa mencemari lingkungan.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <!--// Signle Service --> 
+
+                    </div>
+                    
+                  
+                </div>
+            </section>
+        </main>
