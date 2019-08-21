76bad27a7b0add4736e28ea97c76c4113d6284e8
Unknown <astefanus44@gmail.com>
2019-08-22 01:41:00 +0700

    [ADD] about us page

--- application/views/V_aboutUs.php
+++ application/views/V_aboutUs.php
@@ -1,47 +1,35 @@
-
-
-     
-         
-     
-
-    
-
-        <!-- Breacrumb Area -->
-        <div class="breadcrumb-area" data-black-overlay="7">
+<div class="breadcrumb-area" data-black-overlay="7">
             <div class="container">
                 <div class="row">
                     <div class="col-xl-8 offset-xl-2 col-lg-10 offset-lg-1">
                         <div class="cr-breadcrumb text-center">
-                            <h1>About Us</h1>
-                            <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered
-                                alteration passages of Lorem Ipsum available, but the majority</p>
+                            <h1>ABOUT US</h1>
+                           
+                           <h1 class="text-success">PT ALAM HIJAU SELARAS</h1>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-        <!--// Breacrumb Area -->
 
-        <!-- Page Content -->
         <main class="page-content">
 
             <!-- About Area -->
-            <section id="about-area" class="about-area section-padding-lg bg-grey">
+            <section class="about-area bg-white">
                 <div class="container">
                     <div class="row align-items-center">
-                        <div class="col-xl-5 col-lg-6 order-2 order-lg-1">
-                            <div class="about-content">
-                                <h2>No road is long with good company</h2>
-                                <p>There are many variation of passages of Lorem Ipsum available, but the majority have suffered
-                                    alteration passages of Lorem Ipsum available, but the majority</p>
-                                <a href="about-us.html" class="cr-btn">
-                                    <span>Know More</span>
-                                </a>
+                        <div class="col-xl-7 col-lg-6 col-12 order-2 order-lg-1">
+                            <div class="about-content about-content-2">
+                                <h3>PT Alam Hijau Selaras adalah perusahaan pengemasan tisu yang telah memiliki
+pengalaman sejak tahun 2005 dan telah menjadi produsen In-House Tissue Brand
+bagi merek-merek besar indonesia seperti BlueBird, Circle-K, SOGO, Carrefour,
+Alfamart, Giant, Federal Oil, RS Pantai Indah Kapuk.</h3>
+                                
                             </div>
                         </div>
-                        <div class="col-xl-7 col-lg-6 order-1 order-lg-2">
-                            <div class="about-image text-center wow fadeInRight" data-tilt="">
-                                <img src="<?php echo base_url();?>assets/images/about/about-thumb-1.png" alt="about thumb">
+                        <div class="col-xl-5 col-lg-6 offset-lg-0 col-md-8 offset-md-2 col-12 offset-0 order-1 order-lg-2">
+                            <div class="about-thumb about-thumb-3 mt-5">
+                                <img class="wow slideInRight" data-wow-duration="2s" src="<?php echo base_url();?>assets/images/image017.JPG" alt="skill area thumb" style="visibility: visible; animation-duration: 2s; animation-name: slideInRight;">
                             </div>
                         </div>
                     </div>
@@ -49,227 +37,54 @@
             </section>
             <!--// About Area -->
 
-            <!-- Services Area -->
-            <section class="services-area section-padding-lg bg-white">
-                <div class="container">
 
-                    <div class="row">
-                        <div class="col-xl-8 offset-xl-2 col-lg-10 offset-lg-1 col-12 offset-0">
-                            <div class="section-title text-center">
-                                <h6>Believe In Work</h6>
-                                <h2>Our Services</h2>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="row justify-content-center row justify-content-center service-slider-active cr-slider-dots-1">
-
-                        <!-- Signle Service -->
-                        <div class="col-lg-4 col-md-6 col-12">
-                            <div class="service service-style-5">
-                                <div class="service-image">
-                                    <img src="<?php echo base_url();?>assets/images/service/service-thumb-4.jpg" alt="Service Thumb">
-                                </div>
-                                <div class="service-content">
-                                    <h4>
-                                        <a href="service-details.html">Product Research</a>
-                                    </h4>
-                                    <p>There are many variations of passages Lorem Ipsum, but the majority variations of passages
-                                        of Lorem Ipsum
-                                    </p>
-                                    <a href="service-details.html" class="cr-readmore2">Read More</a>
-                                </div>
-                            </div>
-                        </div>
-                        <!--// Signle Service -->
-
-                        <!-- Signle Service -->
-                        <div class="col-lg-4 col-md-6 col-12">
-                            <div class="service service-style-5">
-                                <div class="service-image">
-                                    <img src="<?php echo base_url();?>assets/images/service/service-thumb-5.jpg" alt="Service Thumb">
-                                </div>
-                                <div class="service-content">
-                                    <h4>
-                                        <a href="service-details.html">Customer Support</a>
-                                    </h4>
-                                    <p>There are many variations of passages Lorem Ipsum, but the majority variations of passages
-                                        of Lorem Ipsum
-                                    </p>
-                                    <a href="service-details.html" class="cr-readmore2">Read More</a>
-                                </div>
-                            </div>
-                        </div>
-                        <!--// Signle Service -->
-
-                        <!-- Signle Service -->
-                        <div class="col-lg-4 col-md-6 col-12">
-                            <div class="service service-style-5">
-                                <div class="service-image">
-                                    <img src="<?php echo base_url();?>assets/images/service/service-thumb-6.jpg" alt="Service Thumb">
-                                </div>
-                                <div class="service-content">
-                                    <h4>
-                                        <a href="service-details.html">Software Solution</a>
-                                    </h4>
-                                    <p>There are many variations of passages Lorem Ipsum, but the majority variations of passages
-                                        of Lorem Ipsum
-                                    </p>
-                                    <a href="service-details.html" class="cr-readmore2">Read More</a>
-                                </div>
-                            </div>
-                        </div>
-                        <!--// Signle Service -->
-
-                        <!-- Signle Service -->
-                        <div class="col-lg-4 col-md-6 col-12">
-                            <div class="service service-style-5">
-                                <div class="service-image">
-                                    <img src="<?php echo base_url();?>assets/images/service/service-thumb-1.jpg" alt="Service Thumb">
-                                </div>
-                                <div class="service-content">
-                                    <h4>
-                                        <a href="service-details.html">Financial Planning</a>
-                                    </h4>
-                                    <p>There are many variations of passages Lorem Ipsum, but the majority variations of passages
-                                        of Lorem Ipsum
-                                    </p>
-                                    <a href="service-details.html" class="cr-readmore2">Read More</a>
-                                </div>
-                            </div>
-                        </div>
-                        <!--// Signle Service -->
-
-                        <!-- Signle Service -->
-                        <div class="col-lg-4 col-md-6 col-12">
-                            <div class="service service-style-5">
-                                <div class="service-image">
-                                    <img src="<?php echo base_url();?>assets/images/service/service-thumb-2.jpg" alt="Service Thumb">
-                                </div>
-                                <div class="service-content">
-                                    <h4>
-                                        <a href="service-details.html">Premium support</a>
-                                    </h4>
-                                    <p>There are many variations of passages Lorem Ipsum, but the majority variations of passages
-                                        of Lorem Ipsum
-                                    </p>
-                                    <a href="service-details.html" class="cr-readmore2">Read More</a>
-                                </div>
-                            </div>
-                        </div>
-                        <!--// Signle Service -->
-
-                        <!-- Signle Service -->
-                        <div class="col-lg-4 col-md-6 col-12">
-                            <div class="service service-style-5">
-                                <div class="service-image">
-                                    <img src="<?php echo base_url();?>assets/images/service/service-thumb-3.jpg" alt="Service Thumb">
-                                </div>
-                                <div class="service-content">
-                                    <h4>
-                                        <a href="service-details.html">Ui/Ux Design</a>
-                                    </h4>
-                                    <p>There are many variations of passages Lorem Ipsum, but the majority variations of passages
-                                        of Lorem Ipsum
-                                    </p>
-                                    <a href="service-details.html" class="cr-readmore2">Read More</a>
-                                </div>
-                            </div>
-                        </div>
-                        <!--// Signle Service -->
-
-                    </div>
-                </div>
-            </section>
-            <!--// Services Area -->
-
-            <!-- Skills Area -->
-            <section id="skills-area" class="skills-area section-padding-lg bg-grey">
+            <!-- About Area -->
+            <section class="about-area bg-white">
                 <div class="container">
                     <div class="row align-items-center">
-                        <div class="col-lg-6">
-                            <div class="skills-area-content p-0">
-                                <h2>Learn About Us</h2>
-                                <p>There are many variations of passages of Lorem Ipsum available, but the majority have of
-                                    Lorem Ipsum available, but the majority</p>
-
-                                <div class="cr-progress-bar">
-                                    <h6>Photoshop</h6>
-                                    <div class="progress">
-                                        <div class="progress-bar wow slideInLeft" data-wow-delay="0.2s" data-wow-duration="2s" role="progressbar" style="width: 85%; visibility: visible; animation-delay: 0.2s; animation-name: slideInLeft;"
-                                            aria-valuenow="85" aria-valuemin="0" aria-valuemax="100"></div>
-                                        <span>85%</span>
-                                    </div>
-                                </div>
-
-                                <div class="cr-progress-bar">
-                                    <h6>Web Design</h6>
-                                    <div class="progress">
-                                        <div class="progress-bar wow slideInLeft" data-wow-delay="0.2s" data-wow-duration="2s" role="progressbar" style="width: 68%; visibility: visible; animation-delay: 0.2s; animation-name: slideInLeft;"
-                                            aria-valuenow="68" aria-valuemin="0" aria-valuemax="100"></div>
-                                        <span>68%</span>
-                                    </div>
-                                </div>
-
-                                <div class="cr-progress-bar">
-                                    <h6>Marketing</h6>
-                                    <div class="progress">
-                                        <div class="progress-bar wow slideInLeft" data-wow-delay="0.2s" data-wow-duration="2s" role="progressbar" style="width: 76%; visibility: visible; animation-delay: 0.2s; animation-name: slideInLeft;"
-                                            aria-valuenow="76" aria-valuemin="0" aria-valuemax="100"></div>
-                                        <span>76%</span>
-                                    </div>
-                                </div>
+                    <div class="col-xl-5 col-lg-6 offset-lg-0 col-md-8 offset-md-2 col-12 ">
+                            <div class="about-thumb about-thumb-3 mt-5">
+                                <img class="wow slideInRight" data-wow-duration="2s" src="<?php echo base_url();?>assets/images/image027.JPG" alt="skill area thumb" style="visibility: visible; animation-duration: 2s; animation-name: slideInRight;">
                             </div>
                         </div>
-                        <div class="col-lg-6">
-                            <div class="video-box m-0 ml-xl-5" data-tilt>
-                                <div class="video-box-thumb">
-                                    <img src="<?php echo base_url();?>assets/images/video-thumbs/video-thumb-1.jpg" alt="video-thumb">
-                                </div>
-                                <a href="https://www.youtube.com/watch?v=dk9uNWPP7EA" class="play-button">
-                                    <i class="flaticon-play-button"></i>
-                                </a>
+                        <div class="col-xl-7 col-lg-6 col-12 order-2 order-lg-1">
+                            <div class="about-content about-content-2">
+                                <h3>Pada tahun 2019 ini, kami juga mulai merintis merek dagang kami sendiri yaitu Caris. Berdasarkan komitmen untuk memberikan kualitas pengemasan terbaik 7 dengan harga terbaik, kami siap untuk masuk ke pasar persaingan sehat industri pengemasan nasional.</h3>
+                                
+                            </div>
+                        </div>
+                       
+                    </div>
+                </div>
+            </section>
+            <!--// About Area -->
+           
+  <!-- About Area -->
+  <section class="about-area bg-white">
+                <div class="container">
+                    <div class="row align-items-center">
+                        <div class="col-xl-7 col-lg-6 col-12 order-2 order-lg-1">
+                            <div class="about-content about-content-2">
+                                <h3>Dengan dibentuknya tim swa-distribusi langsung dari pabrik, kami menjamin
+kualitas produk yang sampai ke tangan Anda adalah yang terbaik dari kami.
+Kebahagiaan kami adalah apabila kami dipercaya untuk berpartisipasi dalam
+menjaga kebersihan dan kesehatan seluruh keluarga Anda.</h3>
+                                
+                            </div>
+                        </div>
+                        <div class="col-xl-5 col-lg-6 offset-lg-0 col-md-8 offset-md-2 col-12 offset-0 order-1 order-lg-2">
+                            <div class="about-thumb about-thumb-3 mt-5 mb-5">
+                                <img class="wow slideInRight" data-wow-duration="2s" src="<?php echo base_url();?>assets/images/image022.JPG" alt="skill area thumb" style="visibility: visible; animation-duration: 2s; animation-name: slideInRight;">
                             </div>
                         </div>
                     </div>
                 </div>
             </section>
-            <!--//Skills Area  -->
+            <!--// About Area -->
+     
 
-            <!-- Counter Area -->
-            <div class="counter-area bg-theme">
-                <div class="counter-area-inner bg-theme">
-                    <div class="container">
-                        <div class="row no-gutters align-items-center">
-                            <div class="col-lg-3 col-md-12">
-                                <div class="counter-content">
-                                    <h1>The last 9 years</h1>
-                                    <h4>What we have done</h4>
-                                </div>
-                            </div>
-                            <div class="col-lg-3 col-md-4">
-                                <div class="counter text-center">
-                                    <h1 class="counter-active">131</h1>
-                                    <h6>AWARD WIN</h6>
-                                </div>
-                            </div>
-                            <div class="col-lg-3 col-md-4">
-                                <div class="counter text-center">
-                                    <h1 class="counter-active">612</h1>
-                                    <h6>Projects Done</h6>
-                                </div>
-                            </div>
-                            <div class="col-lg-3 col-md-4">
-                                <div class="counter text-center">
-                                    <h1 class="counter-active">465</h1>
-                                    <h6>Satisfied Client</h6>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
                 </div>
-            </div>
-            <!--// Counter Area -->
+            </section>
+            <!--// Testimonial Area -->
 
         </main>
-        <!--// Page Content -->
